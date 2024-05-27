686f6d151b95bdefa23780492d8066d7ed7b75ca
Letícia Tambelli <leticiatambelli2@gmail.com>
2024-05-27 19:16:58 -0300

    adicao da escolha de edicao de cadatro

--- config_adm.php
+++ config_adm.php
@@ -12,6 +12,7 @@
 
     <div class="config-container">
         <button><a href="cadastro_adm.php">Meu Cadastro</a></button>
+        <button><a href="escolhaEditar.php">Editar Cadastro</a></button>
         <button><a href="parceiro.php">Cadastrar Parceiro</a></button>
         <button><a href="adm.php">Cadastrar ADM</a></button>
         <button id="voltar"><a href="painel.php">Voltar</a></button>
